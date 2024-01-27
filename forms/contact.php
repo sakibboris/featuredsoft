0e4130f18ce8e7a73f7d2c2e865ebcf178670d0c
sakibboris <asifuddoula76@gmail.com>
2024-01-27 18:55:42 +0600

    content modification

--- forms/contact.php
+++ forms/contact.php
@@ -1,35 +1,43 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Get form data
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $subject = $_POST["subject"];
+  $message = $_POST["message"];
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'asifuddaola933@gmail.com';
-
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+  // Validate email
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo "invalid_email";
+    exit();
   }
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  // Your Gmail email address
+  $to = "asifuddaola933@gmail.com";
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+  // Set email headers
+  $headers = "From: $name <$email>" . "\r\n" .
+    "Reply-To: $email" . "\r\n" .
+    "Content-Type: text/html; charset=UTF-8" . "\r\n";
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  // Compose the email message
+  $email_message = "
+    <html>
+    <head>
+        <title>$subject</title>
+    </head>
+    <body>
+        <p>$message</p>
+    </body>
+    </html>
+    ";
 
-  echo $contact->send();
-?>
+  // Send email
+  if (mail($to, $subject, $email_message, $headers)) {
+    echo "success";
+  } else {
+    echo "error";
+  }
+} else {
+  echo "Invalid request";
+}
